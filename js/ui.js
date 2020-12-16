9da50c86fe51b6153e76832397226431f41d3541
Nitesh Atkar <niteshatkar@gmail.com>
2020-12-17 01:22:11 +0530

    ui-fix

--- js/ui.js
+++ js/ui.js
@@ -24,6 +24,7 @@ class UI {
         this.feels.innerHTML = `Feels Like ${feels_temp}` + "<sup>°</sup>C";
         const icon = weather.weather[0].icon;
         this.img.setAttribute('src', `assest/weather-icons-svgs/${icon}.svg`);
+        console.log(img);
     }
 
     paintErr() {
