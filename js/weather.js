2a85d06bf644d7e30204bf02fcbe099bf55db3e3
niteshatkar <65178961+niteshatkar@users.noreply.github.com>
2020-12-30 20:10:05 +0530

    test-refactor-code

--- js/weather.js
+++ js/weather.js
@@ -3,10 +3,10 @@
 class Weather {
   constructor(city) {
     this.city = city;
-    this.key = '21bd48b3fa575a5d667fd4d40026351e';
+    //this.key = '21bd48b3fa575a5d667fd4d40026351e';
   }
   async fetchWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.key}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${process.env.WEATHER_API_KEY}`);
 
     const data = await response.json();
     return data;
