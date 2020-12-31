c9248f5bc9a52d887a1864b64f7cc830fa9d1f90
Nitesh Atkar <niteshatkar@gmail.com>
2020-12-31 12:15:11 +0530

    test

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,14 @@
 // Weather Class
+const weather_key = process.env.WEATHER_API_KEY;
+console.log(weather_key);
 
 class Weather {
   constructor(city) {
     this.city = city;
    // this.key = '21bd48b3fa575a5d667fd4d40026351e';
-    this.key = process.env.WEATHER_API_KEY;
   }
   async fetchWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.key}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${weather_key}`);
 
     const data = await response.json();
     return data;
