46f404e7168b9e5ff51e4e35c3575ce41c842cd0
Nitesh Atkar <niteshatkar@gmail.com>
2020-12-31 14:20:43 +0530

    final-build

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,12 @@
 // Weather Class
-require('dotenv').config()
-const weather_key = process.env.WEATHER_API;
-console.log(weather_key);
 
 class Weather {
   constructor(city) {
     this.city = city;
-   // this.key = '21bd48b3fa575a5d667fd4d40026351e';
+    this.key = 'b503611b4dc18300e0f66029bd1e9ea1';
   }
   async fetchWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${weather_key}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.key}`);
 
     const data = await response.json();
     return data;
